9530e3551460ec9a91ab3348c5e2132a07b6a483
Jiseok Oh <jiseok95@gmail.com>
2021-08-27 09:28:15 +0900

    feat: Navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navgation.css";
 
 function Navigation(): JSX.Element {
   return (
-    <div>
+    <div className="navigation">
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
     </div>
